3edcde7a705af325c0653d10a21e938fa89385c4
Jessica <jessica@jmcooper.net>
2018-04-17 15:47:17 +0100

    work in progress IRL skeleton

--- side_grids_camp/IRL.py
+++ side_grids_camp/IRL.py
@@ -11,6 +11,7 @@ import numpy as np
 
 """
 # trajectories =  array of demonstration trajectories, each trajectory is an array of state action pairs
+# Trajectories must all be same length?
 
 
 # feature_matrix = array of feature vectors, each feature vector is associated with the state at that index
@@ -18,7 +19,10 @@ import numpy as np
 
 # transition_probabilities = probability of moving from one state to another given action, 1 for legal moves, 0 for illegal moves
 
+
 # feature_expectations = sum feature vectors of every state visited in every demo trajectory, divide result by the number of trajectories, to get feature expectation (over all states) for an average demo trajectory?
+
+# policy = 
 
 """
 
@@ -47,7 +51,6 @@ def getExampleTrajectories():
     # (each state in traj is state action pair (state, action)
     # Could generate (take num, len, policy)or do by hand here   
     return trajectories
-    
     
 
 def getFeatureMatrix(states, feature_vectors):
@@ -100,10 +103,31 @@ def getSVF(trajectories):
 
 
 def getExpectedSVF(rewards, transition_probability, trajectories):
-    # state visitation frequency
+    # expected state visitation frequencies
     # policy = findPolicy(transition_probability, rewards)
 
-    # 
+    # Get initial state frequencies
+    
+    # initial_state_frequencies = np.zeros(N_STATES)
+    # for each trajectory
+        # get initial state of trajectory (trajectory[0][0], 1st element in 1st state in trajectory)
+        # +1 to initial_state_frequencies at index of that state (so we get an array of frequencies of each state being the initial state)  
+        
+    # initial_state_probabilities = initial_state_frequencies/number of trajectories
+    
+    # (I guess initial_state_probabilities would look like this: [0,1,0,0,0,0...] because our agent always starts in same place? - BUT in dynamic envs with more objects could not be.
+    
+    # expected_svf = np.tile(initial_state_probabilities, (trajectories.shape[1], 1)).T
+    
+    # and then... 
+    # for t in range(1, trajectories.shape[1])
+        # set all except initial svf to 0
+        # expected_svf[:,t] = 0
+        # for each state1, action, state2 (3-loop)
+            # expected_svf[state2, t] += expected_svf[i, t-1] * policy[state1, action]
+            
+    
+    
     
     return expected_svf
     
@@ -114,9 +138,16 @@ def findPolicy(transition_probability, rewards):
     # TODO
     
     # Find optimal policy
-    
-    # pull this out to irl method?
+
     value_function = getOptimalValueFunction(transition_probabilities, reward, conv_threshold)
+    
+    # policy = []
+    
+    # for each state
+        # append action that results in highest possible value according to value_function
+        
+    return policy
+    
     
     
     
@@ -124,6 +155,7 @@ def getOptimalValueFunction(transition_probabilities, reward, conv_threshold):
     
     val_func = np.zeros(n_states)
 
+    # TODO
 
 
 
