c0325aca717432ac72bbf92986db994ec4afaa28
Karol Kubicki <kontokarlosa@yahoo.pl>
2018-04-19 13:42:30 +0100

    added get_state_from_grayscale function

--- side_grids_camp/utils/irl_utils.py
+++ side_grids_camp/utils/irl_utils.py
@@ -1,6 +1,8 @@
 import numpy as np
+import tensorflow as tf
 from ai_safety_gridworlds.environments.side_effects_sokoban import SideEffectsSokobanEnvironment as sokoban_game
 from ai_safety_gridworlds.environments.shared.safety_game import Actions
+from side_grids_camp.agents.dqn import StateProcessor
 
 
 ###
@@ -59,9 +61,9 @@ for pl_i in range(size):
 
 
 # %%
-def pl_box_coords(board):
-    pl_x, pl_y = np.where(board == 2)
-    box_x, box_y = np.where(board == 4)
+def pl_box_coords(board, agent=2, box=4):
+    pl_x, pl_y = np.where(board == agent)
+    box_x, box_y = np.where(board == box)
     return (pl_x[0], pl_y[0], box_x[0], box_y[0])
 
 
@@ -84,6 +86,13 @@ def get_game_at(pl_x, pl_y, box_x, box_y):
 # eee = get_game_at(1,1,4,3)
 # ts = eee.reset()
 # ts.observation['board']
+# %% grayscale sokoban board to state index
+GRAYSCALE_A = 134
+GRAYSCALE_B = 78
+def get_state_from_grayscale(gs_img, gs_a=GRAYSCALE_A, gs_b=GRAYSCALE_B):
+    return board_state_map[pl_box_coords(gs_img, agent=gs_a, box=gs_b)]
+
+
 # %% state transition matrix:
 len(state_board_map)
 def get_state_probs(sb_map, bs_map, actions=4):
@@ -100,9 +109,9 @@ def get_state_probs(sb_map, bs_map, actions=4):
     return state_probs
 
 
-# # %% TESTS:
+# %% TESTS:
 # st_probs = get_state_probs(state_board_map, board_state_map)
-# s = 5
+# s = 34
 # a = 1
 # ss = st_probs[s, a, :].argmax()
 # # # Action codes:
@@ -115,3 +124,22 @@ def get_state_probs(sb_map, bs_map, actions=4):
 # # %%
 # env = get_game_at(*state_board_map[ss])
 # env.reset().observation['board']
+
+# %% Tests to grayscale operations
+# env = get_game_at(3, 4, 1, 2)
+# world_shape = env.observation_spec()['board'].shape
+# sp = StateProcessor(world_shape[0], world_shape[1])
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#
+#     time_step = env.reset()
+#     frame = np.moveaxis(time_step.observation['RGB'], 0, -1)
+#     observation_p = sp.process(sess, frame)
+#     print(observation_p)
+#
+# time_step.observation['board']
+#
+# get_state_from_grayscale(observation_p)
+# pl_box_coords(time_step.observation['board'])
+# board_state_map[(3,4,1,2)]
