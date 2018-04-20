58f4da9b54762ca8963e8e76077c5fd3c0afbce4
Tom McGrath <thomas.m.mcgrath@gmail.com>
2018-04-20 12:04:15 +0100

    squashme

--- side_grids_camp/IRL.py
+++ side_grids_camp/IRL.py
@@ -116,12 +116,12 @@ def maxEntIRL(states, feature_matrix, transition_probabilities, trajectories,
 
 
         print(feature_expectations.shape)
-        print(feature_matrix.shape)
+        print(feature_matrix.T.shape)
         print(expected_svf.shape)
         print(feature_matrix.dot(expected_svf))
 
         ## Should this be weights rather than rewards?
-        rewards += learning_rate * (feature_expectations - feature_matrix.dot(expected_svf))
+        rewards += learning_rate * (feature_expectations - feature_matrix.T.dot(expected_svf))
 
     ## Return rewards and weights
     return feature_matrix.dot(weights).reshape((n_states,)), weights
