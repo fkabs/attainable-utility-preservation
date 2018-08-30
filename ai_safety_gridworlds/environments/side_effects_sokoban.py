d6bd4e8feec42845af399b6a0af6bae8302150e2
Alex Turner <turneral1@grinnell.edu>
2018-08-30 12:24:12 -0700

    side_effects_sokoban.py: - Fixed issue where custom_goal wasn't being converted into a Sprite.Position properly. - Fixed variable naming error for custom_goal.
    environment_helper.py:
    - Added ability to render training live.
    - Merged training code into generate_run_agents(), fixing problem with environment scope messing up custom_goals.
    
    train_display_DQN.py:
    - Fixed statistics storage issue.
    
    dqn.py:
    - Added function to estimate Q-values of observation.
    
    aup.py:
    - Made shell for decision rule.
    - Pivoted towards planning-based framework.
    - Cleaned up code.
    - Fixed issue with penalty scaling.

--- ai_safety_gridworlds/environments/side_effects_sokoban.py
+++ ai_safety_gridworlds/environments/side_effects_sokoban.py
@@ -99,7 +99,7 @@ GAME_FG_COLOURS = dict.fromkeys(GAME_BG_COLOURS.keys(), (0, 0, 0))
 GAME_FG_COLOURS.update(safety_game.GAME_FG_COLOURS)
 
 
-def make_game(environment_data, level, custom_goal=None, game_art=GAME_ART):
+def make_game(environment_data, level, game_art=GAME_ART):
   """Initialises the game.
 
   Args:
@@ -113,11 +113,7 @@ def make_game(environment_data, level, custom_goal=None, game_art=GAME_ART):
   boxes = BOXES if level == 1 else BOX_CHR
   sprites = {c: [BoxSprite, (WALL_CHR + COIN_CHR + boxes.replace(c, ''))]
              for c in boxes}
-  NewSprite = AgentSprite
-  if level == 0 and custom_goal:
-    NewSprite.custom_goal = Sprite.Position(row=custom_goal[0], col=custom_goal[1])
-  sprites[AGENT_CHR] = [NewSprite]
-
+  sprites[AGENT_CHR] = [AgentSprite]
 
   update_schedule = [[c for c in boxes], [COIN_CHR], [AGENT_CHR]]
 
@@ -264,7 +260,7 @@ class SideEffectsSokobanEnvironment(safety_game.SafetyEnvironment):
   """Python environment for the side effects sokoban environment."""
   name = "Sokoban"
 
-  def __init__(self, level=0, custom_goal=None, game_art=GAME_ART):
+  def __init__(self, level=0, game_art=GAME_ART):
     """Builds a `SideEffectsSokoban` python environment.
 
     Args:
@@ -287,7 +283,7 @@ class SideEffectsSokobanEnvironment(safety_game.SafetyEnvironment):
     self.GOAL_REWARD = GOAL_REWARD
 
     super(SideEffectsSokobanEnvironment, self).__init__(
-        lambda: make_game(self.environment_data, level, custom_goal, game_art),
+        lambda: make_game(self.environment_data, level, game_art),
         copy.copy(GAME_BG_COLOURS), copy.copy(GAME_FG_COLOURS),
         value_mapping=value_mapping,
         repainter=rendering.ObservationCharacterRepainter(REPAINT_MAPPING))
--- side_grids_camp/agents/aup.py
+++ side_grids_camp/agents/aup.py
@@ -8,58 +8,53 @@ class AUPAgent():
     """
     name = 'Attainable Utility Preservation'
 
-    def __init__(self, penalties=(), m=8, N=2, impact_unit=1):
+    def __init__(self, penalties=(), m=9, N=2, impact_pct=1):
         """
 
-        :param penalties: reward functions whose shifts in attainable values will be penalized.
-        :param m: the horizon up to which the agent will calculate attainable utilties after each action.
-        :param N: up to this many of ImpactUnit is allowed.
+        :param penalties: Reward functions whose shifts in attainable values will be penalized.
+        :param m: The horizon up to which the agent will calculate attainable utilties after each action.
+        :param N: Scale harshness of penalty: 1/N * penalty term.
+        :param impact_pct: How much to scale the attainable null penalty by when regularizing (standing in for a_signal).
         """
         self.penalties = penalties
         self.m = m
         self.N = N
-        self.impact_unit = impact_unit
+        self.impact_unit = impact_pct
 
         self.attainable = dict()  # memoize the (board, steps_left) reward + penalty values, inclusive of current step
         self.cached_actions = dict()
 
-    def act(self, env, actions=[]):
+    def act(self, env, so_far=[]):
         """Get penalties from brute-force search and choose best penalized action.
 
-        :param actions: the actions up until now (assuming a deterministic environment, this allows cheap restarts)
+        :param so_far: The actions up until now; assuming a deterministic environment, this allows cheap restarts.
         """
-        penalized_rewards = self.penalized_rewards(env, actions)
+        penalized_rewards = [self.penalized_reward(env, action, so_far)
+                             for action in range(env.action_spec().maximum + 1)]
         return np.argmax(penalized_rewards)
 
     def get_actions(self, env, steps_left, so_far=[]):
-        """Figure out the n-step optimal plan.
+        """Figure out the n-step optimal plan, returning it and its return.
 
         :param env: Simulator.
         :param steps_left: >= 1; how many steps to plan over.
         :param so_far: actions taken up until now (used for restart).
         """
-        if env._game_over:
-            return [], 0
-
         current_hash = (str(env.last_observations['board']), steps_left)
         if current_hash not in self.cached_actions:
-            pen_rewards = self.penalized_rewards(env, so_far)
-            if steps_left == 1:
-                return [np.argmax(pen_rewards)], max(pen_rewards)
-
-            # If not last action, figure out best we can do for penalized rewards
-            best_actions, best_v = [], float('-inf')
+            best_actions, best_ret = [], float('-inf')
             for a in range(env.action_spec().maximum + 1):
-                env.step(a)
-
-                actions, v = self.get_actions(env, steps_left-1, so_far + [a])
-                if pen_rewards[a] + v > best_v:
-                    best_actions, best_v = [a] + actions, pen_rewards[a] + v
+                r = self.penalized_reward(env, a, so_far)
+                if steps_left > 0:
+                    actions, ret = self.get_actions(env, steps_left-1, so_far + [a])
+                else:
+                    actions, ret = [], 0
+                if r + ret > best_ret:
+                    best_actions, best_ret = [a] + actions, r + ret
                 self.restart(env, so_far)
 
-            self.cached_actions[current_hash] = best_actions, best_v
-        return self.cached_actions[current_hash]   # TODO figure out why won't go around block?
-    # condition: len(so_far) == 2 and so_far[0] == 2 and so_far[1] == 1
+            self.cached_actions[current_hash] = best_actions, best_ret
+        return self.cached_actions[current_hash]
 
     @staticmethod
     def restart(env, actions):
@@ -69,28 +64,26 @@ class AUPAgent():
             env.step(action)
         return env
 
-    def penalized_rewards(self, env, so_far=[]):
-        """The penalized rewards (according to attainable penalty terms) after each action in the current state.
+    def penalized_reward(self, env, action, so_far=[]):  # TODO heavy penalty for finishing level
+        """The penalized reward for taking the given action in the current state. Steps the environment forward.
 
         :param env: Simulator.
+        :param action: The action in question.
         :param so_far: Actions taken up until now.
         """
-        rewards, penalties = np.zeros(env.action_spec().maximum + 1), \
-                             np.zeros((env.action_spec().maximum + 1, len(self.penalties)))
+        time_step = env.step(action)
+        reward, scaled_penalty = time_step.reward if time_step.reward else 0, 0
+        if self.penalties and action != safety_game.Actions.NOTHING:
+            action_pen = self.attainable_penalties(env, self.m, so_far + [action])
+            self.restart(env, so_far)
+            null_pen = self.attainable_penalties(env, self.m, so_far + [safety_game.Actions.NOTHING])
+            null_sum = sum(null_pen)
+            self.restart(env, so_far + [action])
 
-        for action in range(env.action_spec().maximum + 1):  # non-null actions
-            env.step(action)
-            rewards[action] = env._last_reward
-            # Attainable penalties within m steps after acting
-            penalties[action][:] = self.attainable_penalties(env, self.m, so_far=so_far + [action])
-
-        # Difference of attainable rewards between taking action and doing nothing
-        action_differences = np.array([abs(penalty - penalties[safety_game.Actions.NOTHING])
-                                       for penalty in penalties])
-        weighted_penalties = np.array([sum(diffs) / len(diffs)
-                                       for diffs in action_differences]) / (self.N * self.impact_unit) if self.penalties \
-            else np.zeros(len(rewards))
-        return rewards - weighted_penalties
+            # Scaled difference between taking action and doing nothing
+            scaled_penalty = sum(abs(action_pen - null_pen)) / (len(action_pen) * self.N * sum(null_pen)) if null_sum \
+                else 0
+        return reward - scaled_penalty
 
     def attainable_penalties(self, env, steps_left, so_far=[]):
         """Returns penalty rewards attainable within steps_left steps.
